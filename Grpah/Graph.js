0b5ca5c909a59d9926a73a4351c3d2ebed169cbc
Abhijeet Patil <abhijeetpatil27199@gmail.com>
2024-06-04 00:38:19 +0530

    graph  dfs implementation

--- Grpah/Graph.js
+++ Grpah/Graph.js
@@ -1,65 +1,86 @@
+const { Stack } = require("../Stack/Stack.js");
 class Graph {
-    constructor(){
-        this.adjList = new Array()
+  constructor() {
+    this.adjList = new Array();
+  }
+
+  get(v) {
+    for (var i = 0; i < this.adjList.length; i++) {
+      if (Object.keys(this.adjList[i])[0] === v) {
+        return this.adjList[i][v];
+      }
+    }
+  }
+
+  addEdge(v, w) {
+    // to add edge from v to w
+    this.get(v).push(w);
+
+    // as this is unidirected graph lets add edge from w to v
+    this.get(w).push(v);
+  }
+
+  addVertex(v) {
+    this.adjList.push({ [v]: new Array() });
+  }
+
+  dfs() {
+    const st = new Stack();
+    const visitedArr = [];
+    st.push(Object.keys(this.adjList[0])[0]);
+    visitedArr.push(Object.keys(this.adjList[0])[0]);
+
+    while (!st.isEmpty()) {
+      const adjArrOfTop = this.get(st.stackArr[st.top]);
+      for (var i = 0; i < adjArrOfTop.length; i++) {
+        if (!visitedArr.includes(adjArrOfTop[i])) {
+          st.push(adjArrOfTop[i]);
+          visitedArr.push(adjArrOfTop[i]);
+          break;
+        } else if (
+          visitedArr.includes(adjArrOfTop[i]) &&
+          i === adjArrOfTop.length - 1
+        ) {
+          st.pop();
+        }
+      }
     }
 
-    
+    console.log("DFS -> ", visitedArr);
+  }
 
-    get(v){
-            for(var i=0;i<this.adjList.length;i++){
-                if(Object.keys( this.adjList[i])[0]===v){
-                    return this.adjList[i][v]
-                }
-            }
-    }
-    
-    addEdge(v,w){
-        // to add edge from v to w 
-        this.get(v).push(w)
+  displayGraph() {
+    this.adjList.forEach((k) => {
+      let res = "";
+      Object.keys(k).forEach((key) => {
+        k[key].forEach((val) => {
+          res = res + "  " + val;
+        });
 
-        // as this is unidirected graph lets add edge from w to v
-        this.get(w).push(v)
-    }
-
-    addVertex (v){
-        this.adjList.push({[v]:new Array()})
-    }
-
-    displayGraph(){
-
-        this.adjList.forEach((k)=>{
-            let res = '';
-            Object.keys(k).forEach((key)=>{
-                
-                k[key].forEach((val)=>{
-                    res = res +"  "+ val
-                })
-
-                console.log(key," -> ", res)
-            })
-        })
-       
-    }
-
+        console.log(key, " -> ", res);
+      });
+    });
+  }
 }
 
-const g = new Graph()
+const g = new Graph();
 
-var vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ];
- 
+var vertices = ["A", "B", "C", "D", "E", "F"];
+
 // adding vertices
 for (var i = 0; i < vertices.length; i++) {
-    g.addVertex(vertices[i]);
+  g.addVertex(vertices[i]);
 }
- 
-// adding edges
-g.addEdge('A', 'B');
-g.addEdge('A', 'D');
-g.addEdge('A', 'E');
-g.addEdge('B', 'C');
-g.addEdge('D', 'E');
-g.addEdge('E', 'F');
-g.addEdge('E', 'C');
-g.addEdge('C', 'F');
 
-g.displayGraph()
+// adding edges
+g.addEdge("A", "B");
+g.addEdge("A", "D");
+g.addEdge("A", "E");
+g.addEdge("B", "C");
+g.addEdge("D", "E");
+g.addEdge("E", "F");
+g.addEdge("E", "C");
+g.addEdge("C", "F");
+
+g.displayGraph();
+console.log(g.dfs());
--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,53 +1,56 @@
 class Stack {
+  constructor() {
+    this.size = 0;
+    this.top = -1;
+    this.stackArr = [];
+  }
 
-    constructor(){
-        this.size = 0
-        this.top = -1 
-        this.stackArr = []
-    }
+  push(data) {
+    console.log("pushing data to stack : ", data);
+    this.top++;
+    this.stackArr[this.top] = data;
+    this.size++;
+  }
 
-    push(data){
-        console.log("pushing data to stack : ",data)
-        this.top ++
-        this.stackArr[this.top] = data
-        this.size++
+  pop() {
+    if (this.isEmpty()) {
+      console.log("stack is empty");
+      return;
+    } else {
+      const data = this.stackArr[this.top];
+      console.log("removing data from stack : ", data);
+      this.stackArr.splice(this.top, 1);
+      this.top--;
+      this.size--;
+      return data;
     }
+  }
 
-    pop(){
-        if(this.isEmpty()){
-            console.log("stack is empty")
-            return 
-        }else {
-            const data = this.stackArr[this.top]
-            console.log("removing data from stack : ",data)
-            this.stackArr.splice(this.top,1)
-            this.top--
-            this.size--
-        }
-             
-    }
+  display() {
+    this.stackArr.forEach((ele) => console.log(ele, " "));
+    console.log("top pointer : ", this.stackArr[this.top]);
+  }
 
-    display(){
-        this.stackArr.forEach((ele)=> console.log(ele," "))
-        console.log("top pointer : ",this.stackArr[this.top])
-    }
-
-    isEmpty(){
-        return this.top === -1 ? true : false
-    }
+  isEmpty() {
+    return this.top === -1 ? true : false;
+  }
 }
 
-const stack = new Stack()
-stack.push(1)
-stack.display()
-stack.push(2)
-stack.push(4)
-stack.push(3)
-stack.push(5)
-stack.push(6)
-stack.display()
-stack.pop()
-stack.display()
-stack.push(3)
-stack.display()
-console.log("is empty",stack.isEmpty())
+module.exports = {
+  Stack,
+};
+
+const stack = new Stack();
+stack.push(1);
+stack.display();
+stack.push(2);
+stack.push(4);
+stack.push(3);
+stack.push(5);
+stack.push(6);
+stack.display();
+stack.pop();
+stack.display();
+stack.push(3);
+stack.display();
+console.log("is empty", stack.isEmpty());
